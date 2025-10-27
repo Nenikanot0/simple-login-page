29e52cf4e8f450a5ce030372687a864a53238ba3
Nenikanot0 <meherneharika503@gmail.com>
2025-10-27 15:34:24 +0530

    Initial React project commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import SocialLogin from "./components/socialLogin";
+import InputField from "./components/inputField";
+const App = () => {
+  return (
+    <div className="login-container" ><h3>Log in with</h3>
+      <SocialLogin/>
+      <p className="separator"><span>or</span></p>
+      <form action="#" className="login-form">
+        <InputField type="email" placeholder="example@gmail.com" icon="mail"  /> 
+        <InputField type="password" placeholder="jkLm@2123" icon="lock" className="loginPass" /> 
+        <a href="#" className="forgot" >Forgot password?</a>
+
+        <button className="login-button">Log In</button>
+      </form>
+
+      <p className="signup-text">Don't have an account? <a href="#">Signup now</a> </p>
+    </div>
+
+  )
+}
+
+export default App
---
+++ src/components/SocialLogin.jsx
@@ -0,0 +1,17 @@
+
+const SocialLogin = () => {
+  return (
+    <div className="social-login">
+        <button className="social-button">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/768px-Google_%22G%22_logo.svg.png" alt="" className="social-icon" />
+          Google
+        </button>
+        <button className="social-button">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg" alt="" className="social-icon" />
+          Apple
+        </button>
+      </div>
+  )
+}
+
+export default SocialLogin
